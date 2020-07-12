68df1565bd1ab9a08cc6f04f801b04238b17dc06
Lucas Portela <lucasfjportela@gmail.com>
2020-07-12 17:41:48 -0300

    Configuration of EditorConfig, Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#c96a65" />
+        <Text style={styles.text}>Hey! :D</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c96a65',
+  },
+  text: {
+    fontSize: 18,
+    color: '#fff',
+  },
+});
